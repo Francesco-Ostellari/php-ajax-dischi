bdd6981db10aa3ff4edc31f50b89de5c2619d723
Francesco-Ostellari <francescoostellari@gmail.com>
2022-02-02 14:28:16 +0100

    init

---
+++ index-php.php
@@ -0,0 +1,5 @@
+<!-- Prima Milestone:
+Stampiamo i dischi solo con l’utilizzo di PHP, che stampa direttamente i dischi in pagina: al caricamento della pagina ci saranno tutti i dischi.
+Passate a questa solo dopo aver fatto la prima milestone
+Seconda Milestone:
+Attraverso l’utilizzo di axios: al caricamento della pagina axios chiederà, attraverso una chiamata api, i dischi a php e li stamperà attraverso vue. -->
